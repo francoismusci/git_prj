fde1bcbcb7492cb59cb1087a8614d16257b258f3
Francois <francois.musci@gmail.com>
2025-04-19 17:35:25 +0200

    added features

--- somma.py
+++ somma.py
@@ -1,4 +1,15 @@
+"""
+
+la funzione somma prende in input due numeri e restituisce la loro somma
+
+"""
+
+
 def somma(a,b):
     return a+b
+"""
+il comando print stampa la somma 
 
-print(somma(1,2))
+"""
+
+print(somma(2,3))
