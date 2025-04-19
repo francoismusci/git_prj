b9dd9d18958d5a5383d6c0b8b79a4f5854d23f9e
Francois <francois.musci@gmail.com>
2025-04-19 18:05:54 +0200

    quarto commit

---
+++ differenza.py
@@ -0,0 +1,15 @@
+def differenza(a, b):
+    """
+    Calcola la differenza tra due numeri.
+
+    Args:
+        a (int): Il primo numero.
+        b (int): Il secondo numero.
+
+    Returns:
+        int: La differenza tra a e b.
+    """
+    return a - b
+
+sottrazione = differenza(10, 5)
+print(sottrazione)  # Output: 5
